20b7e126dc6425fd40c620b5fb23660cdfd89a55
Rob Walker <rob@ladle.net>
2020-11-29 20:34:27 -0800

    decode special characters in names - #1

--- src/mcexport.py
+++ src/mcexport.py
@@ -17,42 +17,110 @@ from pathlib import Path as path
 
 DEBUG = 0
 
+def decodeLongName(longName):
+    fileType = "a"
+    shortName = ""
+    if (longName[0] == 'f'):
+        fileType = "function"
+    elif (longName[0] == 's'):
+        fileType = "strategy"
+    elif (longName[0] == 'i'):
+        fileType = "indicator"
+    else:
+        fileType = "other"
+    if (longName[1] != '_'):
+        shortNameDelimiter = longName[1]
+        i = 3
+        while (i < len(longName)):
+            if (DEBUG == 1):
+                print ("longName " + str(i) + " : " + longName[i])
+            if (longName[i] == shortNameDelimiter):
+                shortNameSpecial = longName[i + 1] + longName [i + 2]
+                if (DEBUG == 1):
+                    print ("Special character : " + shortNameSpecial)
+                i = i + 2
+                if (shortNameSpecial == "20"):
+                    shortName = shortName + " "
+                elif (shortNameSpecial == "21"):
+                    shortName = shortName + "!"
+                elif (shortNameSpecial == "23"):
+                    shortName = shortName + "#"
+                elif (shortNameSpecial == "24"):
+                    shortName = shortName + "$"
+                elif (shortNameSpecial == "25"):
+                    shortName = shortName + "%"
+                elif (shortNameSpecial == "26"):
+                    shortName = shortName + "&"
+                elif (shortNameSpecial == "28"):
+                    shortName = shortName + "("
+                elif (shortNameSpecial == "29"):
+                    shortName = shortName + ")"
+                elif (shortNameSpecial == "2c"):
+                    shortName = shortName + ","
+                elif (shortNameSpecial == "2d"):
+                    shortName = shortName + "-"
+                elif (shortNameSpecial == "2e"):
+                    shortName = shortName + "."
+                elif (shortNameSpecial == "2f"):
+                    # '/' could be a directory delimiter, so we will
+                    # substitute an '_'
+                    # shortName = shortName + "/"
+                    shortName = shortName + "_"
+                elif (shortNameSpecial == "3c"):
+                    # '<' isn't working in the filename, so we will
+                    # substitute an 'lt'
+                    # shortName = shortName + "<"
+                    shortName = shortName + "lt"
+                elif (shortNameSpecial == "3e"):
+                    # '<' isn't working in the filename, so we will
+                    # substitute an 'gt'
+                    # shortName = shortName + ">"
+                    shortName = shortName + "gt"
+                else:
+                    print("Unknown special character, please report")
+                    print("this at https://github.com/biffhero/mcExport/issues")
+                    print("Include the special character in the report")
+                    print("report : " + shortNameSpecial)
+                    exit()
+            else:
+                shortName = shortName + longName[i]
+            if (DEBUG == 1):
+                print ("Short name : " + shortName)
+            i = i + 1
+    else:
+        shortName = longName[2:]  
+    if (DEBUG == 1):
+        print("")
+        print("Long Name: ", longName)
+        print(" fileType : ", fileType)
+        print(" Short Name: ", shortName)
+    return fileType, shortName
+
 def main():
     file = sys.argv[1]    
     tree = ET.parse(file)
     root = tree.getroot()
     for gn in root:
-        if DEBUG:
+        if (DEBUG == 1):
             print("gn : ", gn.tag, gn.attrib)
         for gnd in gn:
             if (gnd.tag == "data"):
-                if DEBUG:
+                if (DEBUG == 1):
                     print("gnd data: ", gnd.tag, gnd.attrib)
                     print(" tag: ", gnd.tag)
                     print(" attrib : ", gnd.attrib)
                     print(" first : ", gnd.attrib['first'])
                 longName = gnd.attrib['first']
-                if (longName[0] == 'f'):
-                    fileType = "function"
-                elif (longName[0] == 's'):
-                    fileType = "strategy"
-                elif (longName[0] == 'i'):
-                    fileType = "indicator"
-                else:
-                    fileType = "other"
-                shortName = longName[2:]    
-                if DEBUG:
-                    print(" name : ", shortName)
-                    print(" type : ", fileType)
+                fileType, shortName = decodeLongName(longName)
             elif (gnd.tag == "NodeText"):
-                if DEBUG:
+                if (DEBUG == 1):
                     print("encoded: ", gnd.tag, gnd.attrib)
                     print(" tag: ", gnd.tag)
                     print(" attrib : ", gnd.attrib)
                     print(" text : ", gnd.text)
                 text = gnd.text
                 textProgram = base64.urlsafe_b64decode(text)
-                if DEBUG:
+                if (DEBUG == 1):
                     print(" program : ", textProgram)
                 if not os.path.exists("mc"):
                     os.mkdir("mc")
@@ -79,7 +147,7 @@ def main():
                 fo.close()
             elif (gnd.tag != "data"):
                 1
-                if DEBUG:
+                if (DEBUG == 1):
                     print("gnd other: ", gnd.tag, gnd.attrib)
                     print(" tag: ", gnd.tag)
                     print(" attrib : ", gnd.attrib)
